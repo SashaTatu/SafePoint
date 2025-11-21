ac26a327495960956320461e6785487a82c16ed1
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-11-22 00:53:34 +0200

    норм

--- routes/deviceunieqeRouter.js
+++ routes/deviceunieqeRouter.js
@@ -9,6 +9,5 @@ const deviceunieqeRoutes = express.Router({ mergeParams: true });
 deviceunieqeRoutes.get('/:deviceId/parametersget', deviceParameterGet);
 deviceunieqeRoutes.post('/:deviceId/parameterspost', deviceParameterPost);
 deviceunieqeRoutes.get('/:deviceId/getId', GetDeviceById);
-deviceunieqeRoutes.get('/:deviceId/checkalarm', CheckAlarm);
 
 export default deviceunieqeRoutes;
