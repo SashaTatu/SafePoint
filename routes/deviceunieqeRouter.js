a0d8690c7ceff1f38118e3bf73be8316f912ceab
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-11-21 21:40:57 +0200

    64

--- routes/deviceunieqeRouter.js
+++ routes/deviceunieqeRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { deviceParameterPost, deviceParameterGet, GetDeviceById} from '../controllers/deviceunieqeController.js';
-import userAuth from '../middleware/userAuth.js';
+import { deviceParameterPost, deviceParameterGet, GetDeviceById } from '../controllers/deviceunieqeController.js';
 
 const deviceunieqeRoutes = express.Router();
 
-deviceunieqeRoutes.post('/parameterspost', userAuth, deviceParameterPost);
-deviceunieqeRoutes.get('/parametersget', userAuth, deviceParameterGet);
+
+deviceunieqeRoutes.post('/device/:deviceId/parameters', deviceParameterPost);
+deviceunieqeRoutes.get('/device/:deviceId/parameters', deviceParameterGet);
 deviceunieqeRoutes.get('/getId', GetDeviceById);
 
 export default deviceunieqeRoutes;
