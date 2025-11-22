ee43563bb9f70d859979a8df384420decb68b7f2
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-11-22 02:50:52 +0200

    ск34п

--- controllers/authController.js
+++ controllers/authController.js
@@ -321,20 +321,4 @@ export const getUser = async (req, res) => {
   }
 };
 
-export const GetRegionUID = async (req, res) => {
 
-    const userId = req.userId
-    try {
-        const user = await userModel.findById(userId);
-
-        if (!user) {
-            return res.status(404).json({ success: false, message: 'Користувача не знайдено' });
-        }
-        return res.status(200).json({
-            success: true,
-            regionUID: user.regionUID
-        })
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Помилка отримання regionUID', error });
-    }
-}
