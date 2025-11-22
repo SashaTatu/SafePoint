299e76258581bf98b2458cdf44fde55a6aa04ca3
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-11-22 04:11:32 +0200

    vrgt

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import userAuth from '../middleware/userAuth.js';
-import { getUserData, isAlert, userIsAlert } from '../controllers/userController.js';
+import { getUserData, isAlert, } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/data', userAuth, getUserData);
 userRouter.post('/is-alert', userAuth, isAlert);
-userRouter.get('/isgetalert', userAuth, userIsAlert);
+
 
 export default userRouter;
