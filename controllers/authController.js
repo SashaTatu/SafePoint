0be63cec3044fae0531a55e268df4ca71a13c1bd
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-12-04 22:22:20 +0200

    кіряік

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,19 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = new userModel({ name, email, password: hashedPassword, region });
+        const regionUIDMap = {
+            vinnytska: 4, dnipropetrovska: 9, volynska: 8, donetska: 28,
+            zhytomyrska: 10, zakarpatska: 11, zaporizka: 12, ivanoFrankivska: 13,
+            kyivska: 14, kirovohradska: 15, luhanska: 16, lvivska: 27,
+            mykolaivska: 17, odeska: 18, poltavska: 19, rivnenska: 5,
+            sumska: 20, ternopilska: 21, kharkivska: 22, khersonska: 23,
+            khmelnytska: 3, cherkaska: 24, chernivetska: 26, chernihivska: 25,
+            kyiv: 31, sevastopol: 30, crimea: 29
+        };
+
+        const regionUID = regionUIDMap[region];
+
+        const user = new userModel({ name, email, password: hashedPassword, region, uid: regionUID });
         await user.save();
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
