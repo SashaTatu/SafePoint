525188cdca30c2be3fd1fa0eeeab14311d395c6d
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-12-04 23:35:19 +0200

    vgrea

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,8 +1,11 @@
 import nodemailer from 'nodemailer';
 
+const isSecure = process.env.SMTP_SECURE === 'true';
+
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: isSecure,
     auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD
