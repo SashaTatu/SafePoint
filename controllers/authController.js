5b54c4e3acf417917f6a935c01a0266d009ba1ea
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-12-04 22:34:14 +0200

    мфеук

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,16 +19,42 @@ export const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const regionUIDMap = {
-            vinnytska: 4, dnipropetrovska: 9, volynska: 8, donetska: 28,
-            zhytomyrska: 10, zakarpatska: 11, zaporizka: 12, ivanoFrankivska: 13,
-            kyivska: 14, kirovohradska: 15, luhanska: 16, lvivska: 27,
-            mykolaivska: 17, odeska: 18, poltavska: 19, rivnenska: 5,
-            sumska: 20, ternopilska: 21, kharkivska: 22, khersonska: 23,
-            khmelnytska: 3, cherkaska: 24, chernivetska: 26, chernihivska: 25,
-            kyiv: 31, sevastopol: 30, crimea: 29
+            "vinnytska": 4,
+            "dnipropetrovska": 9,
+            "volynska": 8,
+            "donetska": 28,
+            "zhytomyrska": 10,
+            "zakarpatska": 11,
+            "zaporizka": 12,
+            "ivano-frankivska": 13,
+            "kyivska": 14,
+            "kirovohradska": 15,
+            "luhanska": 16,
+            "lvivska": 27,
+            "mykolaivska": 17,
+            "odeska": 18,
+            "poltavska": 19,
+            "rivnenska": 5,
+            "sumska": 20,
+            "ternopilska": 21,
+            "kharkivska": 22,
+            "khersonska": 23,
+            "khmelnytska": 3,
+            "cherkaska": 24,
+            "chernivetska": 26,
+            "chernihivska": 25,
+            "kyiv": 31,
+            "sevastopol": 30,
+            "crimea": 29
         };
 
+
         const regionUID = regionUIDMap[region];
+
+        if (!regionUID) {
+            return res.status(400).json({ success: false, message: "Невірний регіон" });
+        }
+
 
         const user = new userModel({ name, email, password: hashedPassword, region, uid: regionUID });
         await user.save();
