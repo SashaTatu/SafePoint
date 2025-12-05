d1e383dc32c8adf7c8fc16b6595523746e510053
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-12-05 22:30:27 +0200

    Інтеграція10

--- services/alarmChecker.js
+++ services/alarmChecker.js
@@ -1,28 +1,20 @@
 import axios from "axios";
 
-const clientId = process.env.UA_CLIENT_ID;
-const clientSecret = process.env.UA_CLIENT_SECRET;
+export async function checkRegionAlarm(uid) {
+    try {
+        const response = await axios.get(`https://api.ukrainealarm.com/api/v3/alerts/${uid}`, {
+            headers: {
+                accept: "application/json",
+                Authorization: process.env.ALARM_TOKEN  
+            }
+        });
 
-const auth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
-
-export async function checkRegionAlarm(regionId) {
-  try {
-    const url = `https://api.ukrainealarm.com/api/v3/alerts/${regionId}`;
-
-    const resp = await axios.get(url, {
-      headers: {
-        Authorization: `Basic ${auth}`,
-      },
-    });
-
-    return resp.data;
-  } catch (err) {
-    console.log("UkraineAlarm ERROR:", regionId, err.response?.status);
-    return null;
-  }
+        return response.data;
+    } catch (error) {
+        console.log("UkraineAlarm ERROR:", uid, error.response?.status);
+        return null;
+    }
 }
-
-
 
 
 export default checkRegionAlarm;
